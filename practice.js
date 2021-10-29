7e37387b8a069597841cffaea0b046476cc7671f
David <davidyuen0305@gmail.com>
2021-10-29 12:09:40 -0700

    narcissistic number challenge.

--- practice.js
+++ practice.js
@@ -417,9 +417,55 @@ const uniqueInOrder = (iterable) => {
 // SUM OF ODD NUMBERS GIVEN A TRIANGLE
 const rowSumOddNumbers = (n) => {
     // TODO
-    console.log(Math.pow(n,3));
+    console.log(Math.pow(n,2));
     // or
-    console.log(n*n*n);
+    // console.log(n*n*n);
 }
 
-rowSumOddNumbers(2);
+// rowSumOddNumbers(2);
+
+// DOES MY NUMBER LOOK BIG IN THIS
+
+// A Narcissistic Number is a positive number which is the sum of its own digits, 
+// each raised to the power of the number of digits in a given base. In this Kata, 
+// we will restrict ourselves to decimal (base 10).
+
+// For example, take 153 (3 digits), which is narcisstic:
+
+//     1^3 + 5^3 + 3^3 = 1 + 125 + 27 = 153
+// and 1652 (4 digits), which isn't:
+
+//     1^4 + 6^4 + 5^4 + 2^4 = 1 + 1296 + 625 + 16 = 1938
+// The Challenge:
+
+// Your code must return true or false (not 'true' and 'false') depending upon 
+// whether the given number is a Narcissistic number in base 10. This may be 
+// True and False in your language, e.g. PHP.
+
+// Error checking for text strings or other invalid inputs is not required, 
+// only valid positive non-zero integers will be passed into the function.
+
+const narcissistic = (value) => {
+    // Code me to return true or false
+    // let stringValue = value.toString();
+    let individualValue = value.toString().split('');
+    let lengthOfValue = individualValue.length;
+    let powerOfValue = 0;
+
+    for (let i = 0 ; i < lengthOfValue ; i++) {
+        // console.log(individualValue[i])
+        let pow = Math.pow(individualValue[i], lengthOfValue);
+        // console.log(pow)
+        powerOfValue += pow;
+    }
+
+    console.log(value === powerOfValue ? 'true' : 'false')
+
+    // console.log(individualValue, lengthOfValue);
+
+  }
+  
+
+  narcissistic(1938);
+
+
