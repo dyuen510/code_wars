fbc79f9e64627d2c8b570ce55dd24985f7877d95
David <davidyuen0305@gmail.com>
2021-10-31 14:38:35 -0700

    reversing words with more than 4 letters in a string.

--- practice.js
+++ practice.js
@@ -640,4 +640,37 @@ const addUpSubstr = (str) => {
 // with match example
 // const solve = (s) => Math.max(...s.match(/[^aeiou]+/g).map(x => [...x].reduce((s,v) => s + v.charCodeAt() - 96, 0)))
 
+// Spinning words
 
+// Write a function that takes in a string of one or more words, and returns the same string, 
+// but with all five or more letter words reversed (like the name of this kata).
+
+// Strings passed in will consist of only letters and spaces.
+// Spaces will be included only when more than one word is present.
+// Examples:
+
+// spinWords("Hey fellow warriors") => "Hey wollef sroirraw" 
+// spinWords("This is a test") => "This is a test" 
+// spinWords("This is another test") => "This is rehtona test"
+const spinWords = (string) => {
+    let newStr = '';
+    let splitWords = string.split(' ');
+    for(let i = 0 ; i<splitWords.length; i++){
+        // console.log(splitWords[i]);
+        if(splitWords[i].length > 4){
+            let reverseWord = splitWords[i].split('').reverse().join('');
+            splitWords[i] = reverseWord;
+            
+        }   
+    }
+    console.log(splitWords.join(' '));
+  }
+
+//   spinWords("Hey fellow warriors")
+
+// another example
+// function spinWords(words){
+//     return words.split(' ').map(function (word) {
+//       return (word.length > 4) ? word.split('').reverse().join('') : word;
+//     }).join(' ');
+//   }
