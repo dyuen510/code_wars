ce4afb0e3231e153f8e4f220080a7311ff5dfae3
David <davidyuen0305@gmail.com>
2021-11-02 18:51:33 -0700

    pangram solution.

--- practice.js
+++ practice.js
@@ -871,4 +871,35 @@ const doubleChar = (str) => {
 
 }
 
-doubleChar('aabbccdd');
+// const doubleChar = (str) => str.split("").map(c => c + c).join("");
+
+// doubleChar('aabbccdd');
+// PANGRAM
+// A pangram is a sentence that contains every single letter of the alphabet at least once. For example, 
+// the sentence "The quick brown fox jumps over the lazy dog" is a pangram, because it uses the letters A-Z at least once (case is irrelevant).
+
+// Given a string, detect whether or not it is a pangram. Return True if it is, False if not. Ignore numbers and punctuation.
+
+const isPangram = (str) => {
+    let alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+    let strArr = str.toLowerCase();
+
+    for(let i = 0; i<alphabet.length; i++){
+
+        if(strArr.indexOf(alphabet[i]) < 0){
+            console.log(false);
+        }
+        
+
+    }
+    console.log(true);
+}
+
+// isPangram('hello')
+
+// function isPangram(string){
+//     string = string.toLowerCase();
+//     return "abcdefghijklmnopqrstuvwxyz".split("").every(function(x){
+//       return string.indexOf(x) !== -1;
+//     });
+//   }
